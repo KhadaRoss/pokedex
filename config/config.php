d220414849483f9c20d3e767fecc9ec20ef16ff6
KhadaRoss <tiemindbreaker@gmail.com>
2022-03-17 22:31:33 +0100

    Add database module

--- config/config.php
+++ config/config.php
@@ -32,7 +32,8 @@ $aggregator = new ConfigAggregator([
     \Laminas\Diactoros\ConfigProvider::class,
 
     // Default App module config
-    App\ConfigProvider::class,
+    \App\ConfigProvider::class,
+    \Database\ConfigProvider::class,
 
     // Load application config in a pre-defined order in such a way that local settings
     // overwrite global settings. (Loaded as first to last):
--- src/App/src/ConfigProvider.php
+++ src/App/src/ConfigProvider.php
@@ -4,19 +4,8 @@ declare(strict_types=1);
 
 namespace App;
 
-/**
- * The configuration provider for the App module
- *
- * @see https://docs.laminas.dev/laminas-component-installer/
- */
 class ConfigProvider
 {
-    /**
-     * Returns the configuration array
-     *
-     * To add a bit of a structure, each section is defined in a separate
-     * method which returns an array with its configuration.
-     */
     public function __invoke(): array
     {
         return [
@@ -25,9 +14,6 @@ class ConfigProvider
         ];
     }
 
-    /**
-     * Returns the container dependencies
-     */
     public function getDependencies(): array
     {
         return [
@@ -39,9 +25,6 @@ class ConfigProvider
         ];
     }
 
-    /**
-     * Returns the templates configuration
-     */
     public function getTemplates(): array
     {
         return [
